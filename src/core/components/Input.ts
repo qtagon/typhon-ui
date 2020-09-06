e7ccb3a1373de8b6145b4c0bf2ac0e2b8481ae53
Dorin <dorinm.musteata@gmail.com>
2020-09-06 11:58:34 +0300

    Input types

--- src/core/components/Input.ts
+++ src/core/components/Input.ts
@@ -2,13 +2,26 @@ import { Component } from '../Component';
 
 export class Input extends Component {
   type: string = 'input';
+  typed: string = 'text';
+  description: string = '';
 
   /**
    *
    * @param {string} title
    */
-  constructor(title: string) {
+  constructor(title: string = '', description: string = '') {
     super();
     this.title = title;
+    this.description = description;
+    this.typed = 'text';
+  }
+
+  /**
+   * typed setter
+   * @param {string} typed
+   */
+  public setTyped(typed: string = 'text'): this {
+    this.typed = typed;
+    return this;
   }
 }
